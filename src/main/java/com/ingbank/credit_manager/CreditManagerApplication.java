40028a2c5c3071e8d6d317d0a483c0ada23d06d8
kaydemir <kaydemir@lidionbank.com>
2024-12-22 20:02:05 +0300

    initial commit

---
+++ src/main/java/com/ingbank/credit_manager/CreditManagerApplication.java
@@ -0,0 +1,13 @@
+package com.ingbank.credit_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ingbank/credit_manager/CreditManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ingbank.credit_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
